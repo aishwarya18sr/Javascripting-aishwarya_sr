31ec02f8e0f75b96a7c72d287f1c8ee9e045615a
aishwarya18sr <Aishwarya_S_R@mckinsey.com>
2022-02-10 08:50:45 +0530

    feat:add if statement

---
+++ if-statement.js
@@ -0,0 +1,10 @@
+let fruit = "orange";
+length_fruit = fruit.length;
+if(length_fruit>5)
+{
+    console.log("The fruit name has more than five characters.");
+}
+else
+{
+    console.log("The fruit name has five characters or less.");
+}
