b53f9ac1629f92257b5c79c183d6bc92a45546e8
aishwarya18sr <Aishwarya_S_R@mckinsey.com>
2022-02-11 09:03:12 +0530

    feat: add print amstrong numbers

---
+++ printAmstrongNumbers.js
@@ -0,0 +1,28 @@
+function isAmstrongNumber(num)
+{
+    const actualNum = num;
+    let sum = 0;
+    let lastDigit;
+    const numOfDigits = num.toString().length;
+    while(num>0)
+    {
+        lastDigit = num % 10;
+        sum+= lastDigit**numOfDigits;
+        num = Math.floor(num / 10);
+    }
+    return (sum===actualNum)
+}
+
+function printAmstrongNumber(number)
+{
+    for(let i = 1; i <= number; i++)
+    {
+        if(isAmstrongNumber(i))
+        {
+            console.log(i);
+        }
+    }
+}
+
+const N = 1000;
+printAmstrongNumber(N);
