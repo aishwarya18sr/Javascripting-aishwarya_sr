98c3c0913c3e364f3592e046a01aa5f3a887eaf5
aishwarya18sr <Aishwarya_S_R@mckinsey.com>
2022-02-12 21:17:59 +0530

    fix: invalid input errors

--- checkWordsRepetitionSentence.js
+++ checkWordsRepetitionSentence.js
@@ -1,41 +1,68 @@
-function splitSentenceToWords(givenSentence)
-{
-    const wordsArray = givenSentence.split(" ");
-    return wordsArray;
-}
-
-function isWordsRepeating(wordsArray)
-{
-    let words = new Set();
-    const length_wordsArray = wordsArray.length;
-    for(let i=0; i < length_wordsArray; i++)
+function isWordsRepeating(sentence,delimiter) {
+    if(sentence === delimiter)
     {
-        if(words.has(wordsArray[i]))
-        {
+        return false;
+    }
+    if (!(typeof sentence === 'string' || sentence instanceof String))
+    {
+        throw new Error('Not a valid input type for sentence.');
+    }
+    if (!(typeof delimiter === 'string' || delimiter instanceof String))
+    {
+        throw new Error('Not a valid input type for delimiter.');
+    }
+    const maxStringLength = 2**53 - 1;
+    const sentenceLength = sentence.length;
+    const delimiterLength = delimiter.length;
+    if(sentenceLength > maxStringLength || delimiterLength > maxStringLength)
+    {
+        throw new Error('String size exceeded the limit.');
+    }
+    sentence = sentence.toLowerCase();
+    const words = sentence.split(delimiter);
+    let distinctWords = new Set();
+    const numOfWords = words.length;
+    for(let wordIndex = 0; wordIndex < numOfWords; wordIndex++) {
+        if(distinctWords.has(words[wordIndex])) {
             return true;
         }
-        else
-        {
-            words.add(wordsArray[i]);
-        }
+        distinctWords.add(words[wordIndex]);
     }
     return false;
 }
 
-function checkWordsRepetition(givenSentence)
+function checkWordsRepetition(sentence,delimiter)
 {
-    wordsArray = splitSentenceToWords(givenSentence);
-    if(isWordsRepeating(wordsArray))
+    if (!(typeof sentence === 'string' || sentence instanceof String))
     {
-        console.log('The sentence has repeating words')
+        throw new Error('Not a valid input type for sentence.');
+    }
+    if (!(typeof delimiter === 'string' || delimiter instanceof String))
+    {
+        throw new Error('Not a valid input type for delimiter.');
+    }
+    const maxStringLength = 2**53 - 1;
+    const sentenceLength = sentence.length;
+    const delimiterLength = delimiter.length;
+    if(sentenceLength > maxStringLength || delimiterLength > maxStringLength)
+    {
+        throw new Error('String size exceeded the limit.');
+    }
+    if(isWordsRepeating(sentence,delimiter))
+    {
+        return ('The sentence has repeating words')
     }
     else
     {
-        console.log('The sentence does not have repeating words')
+        return ('The sentence does not have repeating words')
     }
 }
 
-//const givenSentence = 'These words are repeating words';
- const givenSentence = 'These words are repeating';
+//const sentence = 'These words are repeating words';
+//const sentence = ',';
+//console.log(checkWordsRepetition(sentence,','));
 
-checkWordsRepetition(givenSentence);
+module.exports = {
+    isWordsRepeating,
+    checkWordsRepetition
+};
