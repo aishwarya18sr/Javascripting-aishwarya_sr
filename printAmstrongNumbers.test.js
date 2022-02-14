5a05790c39e7ccc87c48a9191514c7870fdf0f32
aishwarya18sr <Aishwarya_S_R@mckinsey.com>
2022-02-14 09:39:41 +0530

    feat: add input limit exceeded testcase

--- printAmstrongNumbers.test.js
+++ printAmstrongNumbers.test.js
@@ -1,23 +1,23 @@
 const amstrong = require('../printAmstrongNumbers');
 
 describe('isAmstrongNumber function', function() {
-    it('should return true', function() {
+    it('should return true if 0 is passed as input', function() {
         const result = amstrong.isAmstrongNumber(0);
         expect(result).toBe(true);
     });
-    it('should return true', function() {
+    it('should return true if a positive number is passed as input', function() {
         const result = amstrong.isAmstrongNumber(153);
         expect(result).toBe(true);
     });
-    it('should return true', function() {
+    it('should return true if a positive number is passed as input', function() {
         const result = amstrong.isAmstrongNumber(370);
         expect(result).toBe(true);
     });
-    it('should return true', function() {
+    it('should return true if a positive number is passed as input', function() {
         const result = amstrong.isAmstrongNumber(371);
         expect(result).toBe(true);
     });
-    it('should return true', function() {
+    it('should return true if a positive number is passed as input', function() {
         const result = amstrong.isAmstrongNumber(407);
         expect(result).toBe(true);
     });
@@ -46,22 +46,27 @@ describe('isAmstrongNumber function', function() {
         () => amstrong.isAmstrongNumber(-5)
         ).toThrow('Negative number is an invalid input.')
       })
+    it('should throw error when integer range exceeded', () => {
+        expect(
+        () => amstrong.isAmstrongNumber(12453678097654334567435)
+        ).toThrow('Input limit exceeded the range')
+      })
 });
 
 describe('printAmstrongNumber function', function() {
-    it('should return amstrong numbers in the given range', function() {
+    it('should return amstrong numbers in the given range if a positive number is passed as input', function() {
         const result = amstrong.printAmstrongNumber(10);
         const resultArray = Array.from(result);
         const expectedArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         expect(resultArray).toEqual(expect.arrayContaining(expectedArray));
     });
-    it('should return amstrong numbers in the given range', function() {
+    it('should return amstrong numbers in the given range if a positive number is passed as input', function() {
         const result = amstrong.printAmstrongNumber(100);
         const resultArray = Array.from(result);
         const expectedArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         expect(resultArray).toEqual(expect.arrayContaining(expectedArray));
     });
-    it('should return amstrong numbers in the given range', function() {
+    it('should return amstrong numbers in the given range if a positive number is passed as input', function() {
         const result = amstrong.printAmstrongNumber(1000);
         const resultArray = Array.from(result);
         const expectedArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407]
@@ -91,5 +96,10 @@ describe('printAmstrongNumber function', function() {
         expect(
         () => amstrong.printAmstrongNumber(-5)
         ).toThrow('Negative number is an invalid input.')
+      })
+    it('should throw error when integer range exceeded', () => {
+        expect(
+        () => amstrong.printAmstrongNumber(12453678097654334567435)
+        ).toThrow('Input limit exceeded the range')
       })
 });
